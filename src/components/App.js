250b23dfff9eb3e5a8d45cf73e0970029f29172f
iulianstave <iulian.staverescu@gmail.com>
2020-01-07 17:56:53 +0200

    Render Playlist inside App with style

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
+
 import React from 'react';
 import Playlist from './Playlist';
-// import actions from '../actions';
+
 
 const App = () => {
+// redux handles the configuration for child components
+// no need to pass values for props  
   return (
-    <div>
-      <Playlist/>
+    <div className="ui containter grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <Playlist />
+        </div>
+        <div className="column eight wide">
+          {/* <SongDetail /> */}
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default App;
--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -2,9 +2,31 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Playlist extends Component {
+  renderList() {
+    return this.props.songs.map(song => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button className="ui button primary">
+              Select
+            </button>
+          </div>
+          <div className="content">
+            {song.title}
+          </div>
+        </div>
+      );
+    });
+  };
+
   render() {
-    return <div>Playlist</div>;
+  return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
-export default connect()(Playlist);
+const mapStateToProps = (state) => {
+  return {songs: state.songs};
+
+}
+
+export default connect(mapStateToProps)(Playlist);
